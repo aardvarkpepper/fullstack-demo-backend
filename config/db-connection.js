869e7caf0713376c4c345779cf9e444ddf340c20
Bryan <bryan0147@gmail.com>
2025-09-08 10:36:09 -0400

    Establish MongoDB connection in server.

---
+++ config/db-connection.js
@@ -0,0 +1,7 @@
+const mongoose = require('mongoose');
+
+mongoose.connect(process.env.MONGO_URI);
+
+mongoose.connection.once('open', () => {
+  console.log(`Connected to MongoDB database: ${mongoose.connection.name}`);
+});
